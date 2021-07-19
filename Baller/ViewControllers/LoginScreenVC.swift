4b11c4b7b11642f9ec1158dfe2be97aac47d3c26
Kadir Emre Yıldırım <kadiremre@icloud.com>
2021-07-20 02:27:36 +0300

    beautified textfields

--- Baller/ViewControllers/LoginScreenVC.swift
+++ Baller/ViewControllers/LoginScreenVC.swift
@@ -9,11 +9,12 @@ import UIKit
 import TransitionButton
 
 class LoginScreenVC: UIViewController {
-    @IBOutlet var signInButton: TransitionButton!
     
+    @IBOutlet var signInButton:      TransitionButton!
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
-    
+    @IBOutlet var usernameImageView: UIImageView!
+    @IBOutlet var passwordImageView: UIImageView!
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = true
@@ -21,17 +22,27 @@ class LoginScreenVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTextFieldBackground(imageViewName: usernameImageView)
+        configureTextFieldBackground(imageViewName: passwordImageView)
+        placeholderColor(textfieldName: usernameTextField, defaultPlaceholder: "Username")
+        placeholderColor(textfieldName: passwordTextField, defaultPlaceholder: "Password")
         
-//        configureTextField(textFieldName: usernameTextField)
-//        configureTextField(textFieldName: passwordTextField)
- 
     }
     
-//    func configureTextField(textFieldName: UITextField){
-//        textFieldName.layer.cornerRadius = 60
-//        textFieldName.layer.borderWidth = 20
-//        textFieldName.layer.borderColor = UIColor.systemBlue.cgColor
-//    }
+    func configureTextFieldBackground(imageViewName: UIImageView){
+    
+        imageViewName.layer.cornerRadius = 15
+        imageViewName.layer.borderWidth = 3
+    
+        imageViewName.backgroundColor = UIColor(#colorLiteral(red: 0.9564905763, green: 0.936865747, blue: 0.8072264194, alpha: 0.8001280526))
+        imageViewName.layer.borderColor = CGColor(#colorLiteral(red: 0.8251280785, green: 0.8719809651, blue: 0.7639333606, alpha: 1))
+    }
+    
+    func placeholderColor(textfieldName: UITextField, defaultPlaceholder: String) {
+        textfieldName.attributedPlaceholder = NSAttributedString(string: textfieldName.placeholder ?? "\(defaultPlaceholder)", attributes: [NSAttributedString.Key.foregroundColor : UIColor.black])
+    }
+    
+        
     
     @IBAction func signInButtonTapped(_ sender: TransitionButton) {
         signInButton.startAnimation()
